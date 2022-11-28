431b9ea7b9bb4818c5a39b525c3a25ff6aa90300
rupesh palve <rupesh.palve48@outlook.com>
2022-11-28 17:40:30 -0500

    Add project files.

---
+++ CURDusingADO/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CURDusingADO.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
